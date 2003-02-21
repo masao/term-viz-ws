34934025fa66578211d4a6605fc2b9c2c6370720
masao <tmasao@acm.org>
2003-02-21 08:08:39 +0000

    メソッド名変更。

--- client.rb
+++ client.rb
@@ -6,6 +6,6 @@ require 'soap/wsdlDriver'
 MY_WSDL = 'http://nile.ulis.ac.jp/~masao/term-viz-ws/term-viz.wsdl'
 
 obj = SOAP::WSDLDriverFactory.new(MY_WSDL).createDriver
-result = obj.wordSearch("bababa", "hogehoge")
+result = obj.doWordSearch("bababa", "hogehoge")
 
-p result
+puts result
