450f84c1121ffbf39e73f5a79a2fe21fba2490ee
masao <tmasao@acm.org>
2003-03-12 14:17:51 +0000

    doWordSearch 用のクライアントコードを書いた。
    ログも取るようにした。→ soap_*.xml, soap-log

--- client.rb
+++ client.rb
@@ -1,11 +1,29 @@
-#!/usr/local/bin/ruby -Ku
+#!/usr/local/bin/ruby
 # $Id$
 
 require 'soap/wsdlDriver'
+require 'uconv'
 
 MY_WSDL = 'http://nile.ulis.ac.jp/~masao/term-viz-ws/term-viz.wsdl'
 
-obj = SOAP::WSDLDriverFactory.new(MY_WSDL).createDriver
-result = obj.doWordSearch("鎯呭牨")
+def search (key)
+   obj = SOAP::WSDLDriverFactory.new(MY_WSDL).createDriver
+   # obj.resetStream
+   obj.instance_eval { @handler }.instance_eval { @client }.reset(MY_WSDL)
+   obj.setWireDumpFileBase("soap")
+   obj.setWireDumpDev(File.open("soap-log", "w"))
+   obj.doWordSearch(key)
+end
 
-p result
+def usage
+   puts "USAGE:  #{$0} keyword ..."
+   exit
+end
+
+usage() if ARGV.size == 0
+
+ARGV.each do |key|
+   result = search(Uconv.euctou8(key))
+   puts "窗链办米: #{result.exactMatchElements.size} 凤"
+   puts "婶尸办米: #{result.substrMatchElements.size} 凤"
+end
