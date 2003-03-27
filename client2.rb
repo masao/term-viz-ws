1a1fb004f630bcd749195adc6aa9ca9f57bb2af1
masao <tmasao@acm.org>
2003-03-27 05:53:49 +0000

    wireDump を soap-log2 にした。

--- client2.rb
+++ client2.rb
@@ -9,5 +9,6 @@ MY_WSDL = 'http://nile.ulis.ac.jp/~masao/term-viz-ws/term-viz.wsdl'
 obj = SOAP::WSDLDriverFactory.new(MY_WSDL).createDriver
 obj.resetStream
 obj.setWireDumpFileBase("soap")
-obj.setWireDumpDev(File.open("soap-log", "w"))
+obj.setWireDumpDev(File.open("soap-log2", "w"))
+
 result = obj.getWordList("47888")
