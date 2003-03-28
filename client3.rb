d4aca92e29afc878e4981474efaf08b1b53786dc
masao <tmasao@acm.org>
2003-03-28 05:00:43 +0000

    GC.disable して BUS ERROR を吐かなくなったので、とりあえず commit.

--- client3.rb
+++ client3.rb
@@ -1,36 +1,40 @@
 #!/usr/local/bin/ruby -Ku
 # $Id$
 
+GC.disable	# Ruby1.6系ではなぜか BUS ERROR で落ちるので必要。
+
 require 'GraphViz'
 require 'soap/wsdlDriver'
 require 'uconv'
 
-#TERM_WSDL     = 'http://nile.ulis.ac.jp/~masao/term-viz-ws/term-viz.wsdl'
+TERM_WSDL     = 'http://nile.ulis.ac.jp/~masao/term-viz-ws/term-viz.wsdl'
 GRAPHVIZ_WSDL = 'http://nile.ulis.ac.jp/~masao/term-viz-ws/graphviz.wsdl'
 
-#obj = SOAP::WSDLDriverFactory.new(TERM_WSDL).createDriver
-#wordlist = obj.getWordList("47888")
-#STDERR.puts("getWordList")
+term = SOAP::WSDLDriverFactory.new(TERM_WSDL).createDriver
+wordlist = term.getWordList("47888")
+STDERR.puts("getWordList done.")
 
-parent = NodeArray.new()
-parent << Node.new("アウトプット", "2913")
-parent << Node.new("検索", "29007")
-child = NodeArray.new()
-child << Node.new("光学的情報検索", "30645")
-child << Node.new("選択的情報検索", "55142")
-child << Node.new("文献情報検索", "95638")
-word = Word.new("情報検索", "47888", "", parent, child)
-wordlist = WordArray.new()
-wordlist.push(word)
+#  parent = NodeArray.new()
+#  parent << Node.new("アウトプット", "2913")
+#  parent << Node.new("検索", "29007")
+#  child = NodeArray.new()
+#  child << Node.new("光学的情報検索", "30645")
+#  child << Node.new("選択的情報検索", "55142")
+#  child << Node.new("文献情報検索", "95638")
+#  word = Word.new("情報検索", "47888", "", parent, child)
+#  wordlist = WordArray.new()
+#  wordlist.push(word)
 
 obj = SOAP::WSDLDriverFactory.new(GRAPHVIZ_WSDL).createDriver
+STDERR.puts "createDriver done."
 obj.generateEncodeType = true
 obj.resetStream
 obj.setWireDumpFileBase("soap")
 obj.setWireDumpDev(File.open("soap-log3", "w"))
 
-p wordlist
+STDERR.puts "before obj.doGraphViz"
 result = obj.doGraphViz(wordlist, "LR", "png")
-open("doGraphViz.dot", "w") {|f|
+STDERR.puts "end obj.doGraphViz"
+open("soap_doGraphViz.dot", "w") {|f|
    f.print result
 }
