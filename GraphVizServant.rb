b6bcb0a0713158dc4373133058d708ffc3e3ed2d
masao <tmasao@acm.org>
2003-03-29 02:44:02 +0000

    dot_format で format にしたがって、PNGなどに変換して返せるようにした。

--- GraphVizServant.rb
+++ GraphVizServant.rb
@@ -16,25 +16,60 @@ class GraphVizPort
   #    N/A
   #
   def doGraphViz( wordlist, rankdir, format )
-     return SOAP::SOAPBase64.new(wordlist.to_dot(rankdir))
+     dot = wordlist.to_dot(rankdir)
+     result = dot_format(dot, format)
+     return SOAP::SOAPBase64.new(result)
   end
 end
 
 class WordArray
-   def to_dot (rankdir = nil)
+   def to_dot (rankdir = "TB")
       retstr = "digraph G {\n"
       retstr << "  graph [rankdir=\"#{rankdir}\"];\n" if rankdir
       self.each do |word|
 	 retstr << "  #{word.id} [label=\"#{word.name}\"];\n"
 	 word.child.each do |node|
 	    retstr << "  #{node.id} [label=\"#{node.name}\"];\n"
-	    retstr << "  #{word.id} -> #{node.id}\n"
+	    retstr << "  #{word.id} -> #{node.id};\n"
 	 end
 	 word.parent.each do |node|
 	    retstr << "  #{node.id} [label=\"#{node.name}\"];\n"
-	    retstr << "  #{node.id} -> #{word.id}\n"
+	    retstr << "  #{node.id} -> #{word.id};\n"
 	 end
       end
       retstr << "}\n"
    end
+end
+
+def dot_format (str, format = "png")
+   ENV["LD_LIBRARY_PATH"] = "/usr/local/lib"
+   ENV["DOTFONTPATH"] = "."
+   cmd = open("|/usr/local/bin/dot -T#{format} -Nfontname=kochi", "r+")
+   cmd.print str
+   cmd.close_write
+   cmd.read
+end
+
+# TEST
+if $0 == __FILE__
+   test_dot = <<-EOF
+digraph G {
+  1 -> 2 -> 3
+}
+   EOF
+   dotted_dot = <<-EOF
+digraph G {
+	node [fontname=kochi, label="\\N"];
+	graph [bb="0,0,54,196"];
+	1 [pos="27,170", width="0.75", height="0.50"];
+	2 [pos="27,98", width="0.75", height="0.50"];
+	3 [pos="27,26", width="0.75", height="0.50"];
+	1 -> 2 [pos="e,27,116 27,152 27,144 27,135 27,126"];
+	2 -> 3 [pos="e,27,44 27,80 27,72 27,63 27,54"];
+}
+   EOF
+   # p dot_format(test_dot, "dot")
+   # p dotted_dot
+   p (dot_format(test_dot, "dot") == dotted_dot)
+   
 end
